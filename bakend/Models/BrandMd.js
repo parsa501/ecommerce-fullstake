03d3b5345f0dbc103217764df9bdec2c6b5a9fc3
parsa501 <safazadehparsa@gmail.com>
2025-10-12 15:53:05 -0500

    Add Mongoose brand model with title, image, and publish status fields

---
+++ bakend/Models/BrandMd.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const brandSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "عنوان برند الزامی است"],
+      unique: [true, "عنوان برند باید یکتا باشد"],
+      trim: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+export default Brand;
