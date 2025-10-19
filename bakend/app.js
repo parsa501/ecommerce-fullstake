5b85b4dd19b0b42eed6dbe90686013f626dbdbdd
parsa501 <safazadehparsa@gmail.com>
2025-10-19 15:19:20 -0500

    change app

--- bakend/app.js
+++ bakend/app.js
@@ -16,6 +16,8 @@ import uploadRouter from "./Routes/Upload.js";
 import bannerRouter from "./Routes/banner.js";
 import categoryRouter from "./Routes/Category.js";
 import sliderRouter from "./Routes/Slider.js";
+import isAdmin from "./Middlewares/IsAdmin.js";
+import variantRouter from "./Routes/Variant.js";
 
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
@@ -34,6 +36,7 @@ app.use("/api/upload", uploadRouter);
 app.use("/api/banner", bannerRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/slider", sliderRouter);
+app.use('/api/variants', isAdmin,variantRouter);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use((req, res, next) => {
