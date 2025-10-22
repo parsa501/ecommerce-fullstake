273f757bac11a7400a24f421abcfa52fcc20aedd
parsa501 <safazadehparsa@gmail.com>
2025-10-22 18:01:37 -0500

    change router

--- dashbord/src/Routes/index.jsx
+++ dashbord/src/Routes/index.jsx
@@ -1,20 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout";
-import Posts from "../Pages/Post";
 import Home from "../Pages/Home";
-import Category from "../Pages/Category";
-import GetAllPost from "../Pages/Post/GetAll";
-import UpdatePost from "../Pages/Post/Update";
-import CreatePost from "../Pages/Post/Create";
-import GetAllCategory from "../Pages/Category/GetAll";
-import UpdateCategory from "../Pages/Category/Update";
-import CreateCategory from "../Pages/Category/Create";
 import Login from "../Pages/Login";
 import UserAll from "../Pages/User/GetAll";
 import UpdateUser from "../Pages/User/Update";
 import User from "../Pages/User";
-import Comments from "../Pages/Comments";
-import GetAllComments from "../Pages/Comments/GetAll";
 const router = createBrowserRouter([
   {
     path: "/login",
@@ -43,30 +33,56 @@ const router = createBrowserRouter([
         ],
       },
       {
-        path: "post",
-        element: <Posts />,
+        path: "address",
+        element: <Address />,
         children: [
           {
             index: true,
-            element: <GetAllPost />,
+            element: <GetAllAddress />,
           },
           {
             path: "update/:id",
-            element: <UpdatePost />,
+            element: <UpdateAddress />,
           },
           {
             path: "create",
-            element: <CreatePost />,
+            element: <CreateAddress />,
           },
         ],
       },
       {
-        path: "comments",
-        element: <Comments />,
+        path: "brands",
+        element: <Brands />,
         children: [
           {
             index: true,
-            element: <GetAllComments />,
+            element: <GetAllBrands />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateBrands />,
+          },
+          {
+            path: "create",
+            element: <CreateBrands />,
+          },
+        ],
+      },
+      {
+        path: "banner",
+        element: <Banner />,
+        children: [
+          {
+            index: true,
+            element: <GetAllBanner />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateBanner />,
+          },
+          {
+            path: "create",
+            element: <CreateBanner />,
           },
         ],
       },
@@ -85,6 +101,196 @@ const router = createBrowserRouter([
           {
             path: "create",
             element: <CreateCategory />,
+          },
+        ],
+      },
+      {
+        path: "slider",
+        element: <Slider />,
+        children: [
+          {
+            index: true,
+            element: <GetAllSlider />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateSlider />,
+          },
+          {
+            path: "create",
+            element: <CreateSlider />,
+          },
+        ],
+      },
+      {
+        path: "variants",
+        element: <Variants />,
+        children: [
+          {
+            index: true,
+            element: <GetAllVariants />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateVariants />,
+          },
+          {
+            path: "create",
+            element: <CreateVariants />,
+          },
+        ],
+      },
+      {
+        path: "product",
+        element: <Product />,
+        children: [
+          {
+            index: true,
+            element: <GetAllProduct />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateProduct />,
+          },
+          {
+            path: "create",
+            element: <CreateProduct />,
+          },
+        ],
+      },
+      {
+        path: "product-variants",
+        element: <ProductVariants />,
+        children: [
+          {
+            index: true,
+            element: <GetAllProductVariants />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateProductVariants />,
+          },
+          {
+            path: "create",
+            element: <CreateProductVariants />,
+          },
+        ],
+      },
+      {
+        path: "comments",
+        element: <Comments />,
+        children: [
+          {
+            index: true,
+            element: <GetAllComments />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateComments />,
+          },
+          {
+            path: "create",
+            element: <CreateComments />,
+          },
+        ],
+      },
+      {
+        path: "discount",
+        element: <Discount />,
+        children: [
+          {
+            index: true,
+            element: <GetAllDiscount />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateDiscount />,
+          },
+          {
+            path: "create",
+            element: <CreateDiscount />,
+          },
+        ],
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+        children: [
+          {
+            index: true,
+            element: <GetAllOrders />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateOrders />,
+          },
+          {
+            path: "create",
+            element: <CreateOrders />,
+          },
+        ],
+      },
+      {
+        path: "faq",
+        element: <FAQ />,
+        children: [
+          {
+            index: true,
+            element: <GetAllFAQ />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateFAQ />,
+          },
+          {
+            path: "create",
+            element: <CreateFAQ />,
+          },
+        ],
+      },
+      {
+        path: "testimonial",
+        element: <Testimonial />,
+        children: [
+          {
+            index: true,
+            element: <GetAllTestimonial />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateTestimonial />,
+          },
+          {
+            path: "create",
+            element: <CreateTestimonial />,
+          },
+        ],
+      },
+      {
+        path: "user-question",
+        element: <UserQuestion />,
+        children: [
+          {
+            index: true,
+            element: <GetAllUserQuestion />,
+          },
+          {
+            path: "update/:id",
+            element: <UpdateUserQuestion />,
+          },
+          {
+            path: "create",
+            element: <CreateUserQuestion />,
+          },
+        ],
+      },
+      {
+        path: "report",
+        element: <Report />,
+        children: [
+          {
+            index: true,
+            element: <GetAllReport />,
           },
         ],
       },
