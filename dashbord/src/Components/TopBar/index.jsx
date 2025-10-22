2909a201afb83713911f2816830512cb7adfb942
parsa501 <safazadehparsa@gmail.com>
2025-10-22 14:19:46 -0500

    add TopBar

--- dashbord/src/Components/TopBar/index.jsx
+++ dashbord/src/Components/TopBar/index.jsx
@@ -1,22 +1,30 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../Context/AuthContext";
+import { MdLogout } from "react-icons/md";
 
 export default function TopBar() {
   const { handleToken } = useContext(AuthContext);
 
   return (
-    <header className="bg-white/10 backdrop-blur-lg border-b border-white/20 shadow-md px-6 py-4 flex items-center justify-between rounded-xl mx-4 mt-4">
+    <header
+      dir="rtl"
+      className="bg-white/10 backdrop-blur-lg border-b border-white/20 shadow-md px-6 py-4 flex items-center justify-between rounded-xl mx-4 mt-4"
+    >
       <h2 className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-500">
-        Admin Dashboard 🚀
+        پنل مدیریت
       </h2>
+
       <div className="text-sm text-gray-300 font-medium tracking-wide">
-        Welcome, <span className="text-purple-400 font-semibold">Admin</span>
+        خوش آمدید،{" "}
+        <span className="text-purple-400 font-semibold">مدیر</span>
       </div>
+
       <button
         onClick={() => handleToken(null)}
-        className="px-5 py-2.5 rounded-lg font-bold text-white bg-gradient-to-r from-red-500 to-pink-600 shadow-lg hover:scale-105 active:scale-95 transition-all duration-300"
+        className="flex items-center  gap-2 px-5 py-2.5 rounded-lg font-bold text-white bg-gradient-to-r from-red-500 to-pink-600 shadow-lg hover:scale-105 active:scale-95 transition-all duration-300"
       >
-        Logout
+        <MdLogout className="text-lg" />
+        خروج
       </button>
     </header>
   );
