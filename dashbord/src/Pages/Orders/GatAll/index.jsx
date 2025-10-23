02509e3cd941cd7d9ad91b3a6d679166bcee35c6
parsa501 <safazadehparsa@gmail.com>
2025-10-22 19:38:48 -0500

    add order

---
+++ dashbord/src/Pages/Orders/GatAll/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GetAllOrders() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ dashbord/src/Pages/Orders/Update/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UpdateOrders() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
