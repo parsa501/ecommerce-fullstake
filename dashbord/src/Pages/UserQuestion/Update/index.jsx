031befbbb21434b09b6a91f2e34274b544dde3c7
parsa501 <safazadehparsa@gmail.com>
2025-10-22 19:52:32 -0500

    add UserQuestion

---
+++ dashbord/src/Pages/UserQuestion/Update/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UpdateUserQuestion() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ dashbord/src/Pages/UserQuestion/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserQuestion() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
