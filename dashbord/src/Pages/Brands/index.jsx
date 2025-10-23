8b5f9322af7f546e3403facb872df05261009291
parsa501 <safazadehparsa@gmail.com>
2025-10-23 11:56:17 -0500

    add Brands

--- dashbord/src/Pages/Brands/index.jsx
+++ dashbord/src/Pages/Brands/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function Brands() {
   return (
-    <div>
-      
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">مدیریت برندها</h2>
+      <Outlet />
     </div>
-  )
+  );
 }
