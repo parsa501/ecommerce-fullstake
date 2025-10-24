d8da62c3db9b775e7887816b3a1dcafa8331e624
parsa501 <safazadehparsa@gmail.com>
2025-10-24 12:47:09 -0500

    add assests

---
+++ clint/src/assets/assest.js
@@ -0,0 +1,34 @@
+import Flash from "./Flash.png";
+import UnitedDeal from "./United-deal.png";
+import Accessories from "./Accessories.png";
+import BrowseAllProduct from "./Browse-All-Product.png";
+import AppStore from "./AppStore.png";
+import GooglePlay from "./GooglePlay.png";
+import Game from "./Game.png";
+import fasted1 from "./fasted1.png";
+import fasted2 from "./fasted2.png";
+import fasted3 from "./fasted3.png";
+import fasted4 from "./fasted4.png";
+import brand1 from "./Brand1.png";
+import brand2 from "./Brand2.png";
+import brand3 from "./Brand3.png";
+import NotFoundIllustration from "./NotFoundIllustration.jfif"
+import login from "./Login.png"
+import Profile from "./profile.png"
+export const assets = {
+  Flash,
+  UnitedDeal,
+  Accessories,
+  BrowseAllProduct,
+  AppStore,
+  GooglePlay,
+  Game,
+  fasted1,
+  fasted2,
+  fasted3,
+  fasted4,
+  brand1,
+  brand2,
+  brand3,NotFoundIllustration,
+  login,Profile
+};
