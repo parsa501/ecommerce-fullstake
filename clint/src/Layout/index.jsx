2aafd67eaaf1b367c0400c3d33a2e910151e2cff
parsa501 <safazadehparsa@gmail.com>
2025-10-25 15:30:48 -0500

    add layeout

--- clint/src/Layout/index.jsx
+++ clint/src/Layout/index.jsx
@@ -4,13 +4,16 @@ import Navbar from "../Components/Navbar";
 import Footer from "../components/Footer";
 
 export default function Layout() {
-  return (
+   return (
     <>
       <Navbar />
-      <main className="min-h-[30vh] sm:min-h-[40vh] md:min-h-[50vh] lg:min-h-[60vh] xl:min-h-[80vh]">
+      <main
+        dir="rtl"
+        className="min-h-[30vh] sm:min-h-[40vh] md:min-h-[50vh] lg:min-h-[60vh] xl:min-h-[80vh] font-[Vazir]"
+      >
         <Outlet />
       </main>
-      <Footer />
+      <Footer/>
     </>
   );
 }
