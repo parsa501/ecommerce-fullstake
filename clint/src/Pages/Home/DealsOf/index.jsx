cbf0aba15effd93d00f954f63ce6cae3cac9a228
parsa501 <safazadehparsa@gmail.com>
2025-10-26 15:55:49 -0500

    change product card

--- clint/src/Pages/Home/DealsOf/index.jsx
+++ clint/src/Pages/Home/DealsOf/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import fetchData from "../../../Utils/fetchData";
 import DealsOfCard from "./DealsOfCard";
 import DealsOfCardSkeleton from "./DealsOfCardSkeleton";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function DealsOf() {
   const [product, setProduct] = useState();
@@ -10,29 +10,37 @@ export default function DealsOf() {
   useEffect(() => {
     (async () => {
       const response = await fetchData(
-        "products?populate=*&sort=offer:desc&pagination[limit]=4"
+        "product-variants?populate=productId,variantId&sort=discount:desc&pagination[limit]=4"
+
       );
+      console.log(response)
       setProduct(response?.data);
     })();
   }, []);
-  const navigate=useNavigate
+  const navigate = useNavigate;
   const items = product?.map((e) => (
     <DealsOfCard
       key={e?.id}
-      documentId={e?.documentId}
-      name={e?.name}
+      productId={e?.productId?._id}
+      title={e?.productId?.title}
       price={e?.price}
       rating={e?.rating}
-      img={import.meta.env.VITE_BASE_FILE + e?.images[0]?.url}
-      offer={e?.offer}
+      img={import.meta.env.VITE_BASE_FILE + e?.productId?.images?.[0]}
+      discount={e?.discount}
+      priceAfterDiscount={e?.priceAfterDiscount}
     />
   ));
-  if (!product) return <DealsOfCardSkeleton/>
+  if (!product) return <DealsOfCardSkeleton />;
   return (
     <div className="px-[5%] py-10">
-        <div className="flex justify-between  flex-col sm:flex-row gap-4">
-      <h2 className="text-[34px] font-bold">Today’s Deals of the day</h2>
-      <button onClick={()=>navigate('/products')} className="rounded-[8px] cursor-pointer  bg-[#FCBD01] text-[14px] py-4 px-8 font-medium">VIEW ALL</button>
+      <div className="flex justify-between  flex-col sm:flex-row gap-4">
+        <h2 className="text-[34px] font-bold">Today’s Deals of the day</h2>
+        <button
+          onClick={() => navigate("/products")}
+          className="rounded-[8px] cursor-pointer  bg-[#FCBD01] text-[14px] py-4 px-8 font-medium"
+        >
+          VIEW ALL
+        </button>
       </div>
       <div className="flex flex-wrap gap-4 mt-4 mx-auto">{items}</div>
     </div>
