30d6581944a7c4e0ac2598f14510db751399ab5a
parsa501 <safazadehparsa@gmail.com>
2025-10-27 06:25:06 -0500

    add brand famest

--- clint/src/Pages/Home/TopElectronicsBrands/index.jsx
+++ clint/src/Pages/Home/TopElectronicsBrands/index.jsx
@@ -4,29 +4,29 @@ import { assets } from "../../../assets/assest";
 export default function TopElectronicsBrands() {
   return (
     <div className="px-[5%]">
-      <h2 className="text-[34px] font-bold border-b border-gray-300 pb-4 mb-8">
-        TOP <span className="text-[#FCBD01]">ELECTRONICS BRANDS</span>
+      <h2 className="text-[28px] sm:text-[34px] font-bold border-b border-gray-300 pb-4 mb-8 text-center sm:text-right">
+        برندهای برتر <span className="text-[#FCBD01]">الکترونیکی</span>
       </h2>
-      <div className="flex gap-4 justify-center">
-        <div className="w-[400px] h-[200px]">
+      <div className="flex gap-4 justify-center flex-wrap">
+        <div className="w-[380px] h-[200px]">
           <img
-            className=" w-full h-full object-fill "
+            className="w-full h-full object-fill rounded-lg"
             src={assets.brand3}
-            alt="brand1"
+            alt="برند ۱"
           />
         </div>
-        <div className="hidden sm:block w-[400px] h-[200px]">
+        <div className="hidden sm:block w-[380px] h-[200px]">
           <img
-            className=" w-full h-full object-fill "
+            className="w-full h-full object-fill rounded-lg"
             src={assets.brand2}
-            alt="brand1"
+            alt="برند ۲"
           />
         </div>
-        <div className="hidden md:block w-[400px] h-[200px]">
+        <div className="hidden md:block w-[380px] h-[200px]">
           <img
-            className=" w-full h-full object-fill "
+            className="w-full h-full object-fill rounded-lg"
             src={assets.brand1}
-            alt="brand1"
+            alt="برند ۳"
           />
         </div>
       </div>
