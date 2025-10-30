f0604cf5e63a39ffd120910c96af132c645f81e0
parsa501 <safazadehparsa@gmail.com>
2025-10-29 23:37:59 -0500

    add UserQuestions

---
+++ clint/src/Pages/UserQuestions/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserOpinion from "./UserOpinion";
+import Questions from "./Questions";
+
+export default function UserQuestions() {
+  return (
+    <>
+      <UserOpinion />
+      <Questions/>
+    </>
+  );
+}
